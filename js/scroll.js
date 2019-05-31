e5b54878a767950aa2480cbd8629e388f2ced798
Alysha-94 <alysha.iannetta@gmail.com>
2019-05-31 12:40:36 +1000

    additional sections, reusable scroll function

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,5 @@
-function scrollToOne() {
-    var point = document.getElementById('section1');
-    point.scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToTwo() {
-    var point = document.getElementById('section2');
-    point.scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToThree() {
-    var point = document.getElementById('section3');
-    point.scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToFour() {
-    var point = document.getElementById('section4');
-    point.scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToFive() {
-    var point = document.getElementById('section5');
-    point.scrollIntoView({ behavior: 'smooth' });
-}
-function scrollToSix() {
-    var point = document.getElementById('section6');
+function scrollToSection(number) {
+    var sectionNumber = 'section' + number;
+    var point = document.getElementById(sectionNumber);
     point.scrollIntoView({ behavior: 'smooth' });
 }
